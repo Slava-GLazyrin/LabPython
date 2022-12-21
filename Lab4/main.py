947641ddb1d770fd9bf8f97c80e701b94bbcf26c
Slava-GLazyrin <glazyrin_slava2003@mail.ru>
2022-12-21 10:40:53 +0500

    Реализованы функции для генерации списка с ФИО и количеством часов работы, а также функции для записи данных в файл .csv из списка списков и списка словарей

--- Lab4/main.py
+++ Lab4/main.py
@@ -9,7 +9,6 @@ def newMap(func, startList : list) -> list:
         resultList.append(func(i))
     return resultList
 
-#переопределённый reduce
 def newReduce(func, startList : list):
     index : int = 2
     length : int = len(startList)
@@ -18,3 +17,41 @@ def newReduce(func, startList : list):
         result = func(result, startList[index])
         index += 1
     return result
+
+
+#Генерация списка списков с ФИО и количеством часов работы
+def GenerateDataSet() -> list:
+    Surnames : list = ["Возисов", "Глазырин", "Дмитриенко", "Бочкарёв"]
+    Names : list = ["Максим", "Вячеслав", "Андрей", "Макар"]
+    Patronymics : list = ["Петрович", "Вячеславович", "Викторович", "Владиславович"]
+    Persons = list()
+    i : int = 0
+    count : int = 100
+    Persons.append(["ФИО", "Часы работы"])
+    while i < count:
+        randomFullName = Surnames[random.choice(Surnames)] + " " + Names[random.choice(Names)] + " "  + Patronymics[random.choice(Patronymics)]
+        Persons.append([randomFullName, random.randint(10, 350)])
+        i += 1
+    return Persons
+
+#Запись в .csv из списка списков
+def WriteInCSVFromList(dataSet : list, fileName : str):
+     with open(fileName, "w", newline="") as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerows(dataSet)
+
+#Запись в .csv из списка словарей        
+def WriteInCSVFromDict(dataSet : list, fileName : str):
+    with open(fileName, "w", newline="") as file:
+        columns = list(dataSet[0].keys())
+        writer = csv.DictWriter(file, fieldnames=columns, delimiter=',')
+        writer.writeheader()
+        writer.writerows(dataSet)
+
+
+
+
+
+
+
+
